e2ea4c3f87fcb70cb41eecd40a4e2335c94d858d
KamilAkbarovOOP <160862968+KamilAkbarovOOP@users.noreply.github.com>
2024-10-07 19:35:39 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,39 @@
+//************Task 2************
+const max_of_three = (a, b, c) =>{
+    return(Math.max(a, b, c));
+}
+
+console.log(max_of_three(100, 503, 199));
+//*******************************
+
+
+
+//************Task 3************
+const sumTriple = (a, b) => {
+    if(a == b){
+        return (a + b) * 3;
+    }
+    else{
+        return a + b;
+    }
+}
+
+console.log(sumTriple(10, 10));
+console.log(sumTriple(10, 20));
+//******************************
+
+
+
+//************Task 5************
+const first_half = (word) => {
+    if(word.length % 2 == 0){
+        return word.slice(0, word.length / 2);
+    }
+    else{
+        return word;
+    }
+}
+
+console.log(first_half("java"));
+console.log(first_half("PHP"));
+//******************************
